ffcb4e80b84c32e8db2c678383757fea9fcd9749
nico <nico.contigliani@gmail.com>
2021-03-24 18:00:09 -0300

    natbar

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,20 @@ function App() {
   return (
 
 <Fragment>
+<div className="fixed-top">
+  <div className="collapse" id="navbarToggleExternalContent">
+    <div className="bg-dark p-4">
+      <h5 className="text-white h4">Collapsed content</h5>
+      <span className="text-muted">Toggleable via the navbar brand.</span>
+    </div>
+  </div>
+  <nav className="navbar navbar-dark bg-dark">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
+
  <h1>Star Wars </h1>
 
 
